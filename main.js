d7484627240dca5cd3875b66a6b01ee25230f823
sai babu <saibabu@sais-Air-3.domain.name>
2024-03-17 22:50:22 +0530

    Add.js function is added and first sectionTitle is working

---
+++ main.js
@@ -0,0 +1,5 @@
+const section3Title = document.querySelector('.section3-content-sub')
+const section3desc = document.querySelector('.section3-desc')
+section3Title.addEventListener( 'click', () => {
+    section3desc.style.display="block"
+})
